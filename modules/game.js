8dbe0d158856d441bc26661a9b11493040ac0832
Shuri2060 <jeremyho2060@gmail.com>
2019-09-13 00:35:16 +0100

    Update game.js

--- modules/game.js
+++ modules/game.js
@@ -222,3 +222,24 @@ function colCheck(objArr) {
   return col
 }
 /**/
+
+/*DEBUG
+function time(func, n) {
+  return function () {
+    let t = 0
+    for (let i = n; i--;) {
+      let d = performance.now()
+      func.apply(this, arguments)
+      d = performance.now() - d
+      t += d
+    }
+    console.log(t / n)
+  }
+}
+
+var x = []
+for (let i = 0; i < 50000; i++) {
+  x[i] = new Game2D2Object({sx: Math.random() * maxX, sy: Math.random() * maxY, vx: (Math.random() - 0.5) * 20, vy: (Math.random() - 0.5) * 20, ax: (Math.random() - 0.5) * 5, ay: (Math.random() - 0.5) * 5, as: (Math.random() - 0.5) * 4, av: (Math.random() - 0.5) * 10, aa: (Math.random() - 0.5) * 1, r: (Math.random() - 0.5) * 25})
+}
+var test = time(colCheck, 100)
+/**/
