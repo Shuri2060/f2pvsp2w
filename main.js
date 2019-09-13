3465e914c0aed72f0375a72111e94bc5d028a9a4
Shuri2060 <jeremyho2060@gmail.com>
2019-09-13 01:14:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -45,13 +45,13 @@ function main() {
       canvasWidth = canvas.width
       canvasHeight = canvas.height
       
-      var gameCanvasRatio = Math.min(canvasWidth / maxX, canvasHeight / maxY)
-      var gameWidth = maxX * gameCanvasRatio
-      var gameHeight = maxY * gameCanvasRatio
-      var gameLeft = (canvasWidth - gameWidth) / 2
-      var gameRight = gameLeft + gameWidth
-      var gameTop = (canvasHeight - gameHeight) / 2
-      var gameBottom = gameTop + gameHeight
+      gameCanvasRatio = Math.min(canvasWidth / maxX, canvasHeight / maxY)
+      gameWidth = maxX * gameCanvasRatio
+      gameHeight = maxY * gameCanvasRatio
+      gameLeft = (canvasWidth - gameWidth) / 2
+      gameRight = gameLeft + gameWidth
+      gameTop = (canvasHeight - gameHeight) / 2
+      gameBottom = gameTop + gameHeight
     }
     
     //var aspect = canvas.clientWidth / canvas.clientHeight; // needed for later probs
