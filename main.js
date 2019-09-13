72fe7f1c2f1fdb561aa241adaa28fc7f76c35665
Shuri2060 <jeremyho2060@gmail.com>
2019-09-13 01:41:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ function main() {
       ctx.lineWidth = 4
       ctx.arc((gameLeft + objArrI.sx * gameCanvasRatio) | 0, (gameTop + objArrI.sy * gameCanvasRatio) | 0, (objArrI.r * gameCanvasRatio) | 0, 0, Math_2PI)
       ctx.fillStyle = "#FFFFFF"
-      ctx.fill()
+      //ctx.fill()
       ctx.closePath()
     }
     
