0ac61e5978c6e14c6fd87c34c788f14907d15d11
Shuri2060 <jeremyho2060@gmail.com>
2019-09-13 01:22:12 +0100

    Update game.js

--- modules/game.js
+++ modules/game.js
@@ -239,7 +239,7 @@ function time(func, n) {
 
 var x = []
 for (let i = 0; i < 50000; i++) {
-  x[i] = new Game2D2Object({sx: Math.random() * maxX, sy: Math.random() * maxY, vx: (Math.random() - 0.5) * 20, vy: (Math.random() - 0.5) * 20, ax: (Math.random() - 0.5) * 5, ay: (Math.random() - 0.5) * 5, as: (Math.random() - 0.5) * 4, av: (Math.random() - 0.5) * 10, aa: (Math.random() - 0.5) * 1, r: (Math.random() - 0.5) * 25})
+  x[i] = new Game2D2Object({sx: Math.random() * maxX, sy: Math.random() * maxY, vx: (Math.random() - 0.5) * 20, vy: (Math.random() - 0.5) * 20, ax: (Math.random() - 0.5) * 5, ay: (Math.random() - 0.5) * 5, as: (Math.random() - 0.5) * 4, av: (Math.random() - 0.5) * 10, aa: (Math.random() - 0.5) * 1, r: Math.random() * 25})
 }
 var test = time(colCheck, 100)
 /**/
