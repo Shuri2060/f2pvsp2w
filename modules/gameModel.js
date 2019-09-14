c017a4ca4f2e1427b84f67943dd0e48c06793029
Shuri2060 <jeremyho2060@gmail.com>
2019-09-14 13:36:42 +0100

    Update gameModel.js

--- modules/gameModel.js
+++ modules/gameModel.js
@@ -1,1 +1,39 @@
+//For now Leave this stuff as it is
 
+maxX = 1024
+maxY = 1024
+
+grid = []
+gridCellX = 64
+gridCellY = 64
+gridX = maxX / gridCellX
+gridY = maxY / gridCellY
+
+for (let i = 0; i < gridX; i++) {
+  grid[i] = []
+  for (let j = 0; j < gridY; j++) {
+    grid[i][j] = []
+  }
+}
+
+var objArr = []
+//------------------------------------------------
+//Put your setup code here. eg. -
+
+for (let i = 0; i < 50; i++) {
+  objArr[i] = new Game2D2Object({sx: Math.random() * maxX, sy: Math.random() * maxY, vx: (Math.random() - 0.5) * 40, vy: (Math.random() - 0.5) * 40, ax: (Math.random() - 0.5) * 0.01, ay: (Math.random() - 0.5) * 0.01, as: (Math.random() - 0.5) * 4, av: (Math.random() - 0.5) * 10, aa: (Math.random() - 0.5) * 1, r: Math.random() * 100})
+}
+//------------------------------------------------
+//Every frame, the below occurs. All custom code goes in update() and afterColl()
+// update(dt)
+// simPhysics(objArr, dt)
+// const collisions = colCheck(objArr)
+// afterColl(collisions)
+
+function update(dt) { //dt is time in seconds that have passed since last frame
+  
+}
+
+function afterColl(collisions) { //collisions is an array of pairs of objects that collide
+  
+}
